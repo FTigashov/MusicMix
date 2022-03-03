e0169f71b267d62f0e3388ae76bf1ee078afa998
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-03-03 22:03:41 +0300

    Commit 3. Added looping for define music files and inserting them to listBox.

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ import unittest
 
 root = tk.Tk()
 root.title("Music mixer")
-root.geometry("600x800")
+root.geometry("500x400")
+root.config(bg = '#C9C9C9')
 
+rootpath = "D:\\Downloads\music"
+pattern = "*mp3"
+
+listBox = tk.Listbox(root, width = 100, font = "12")
+listBox.pack(padx = 15, pady = 15)
+
+# цикл 1) переходит в директорию 2) ищет файл с расширением mp3
+#print("Запуск цикла по нахождению файлов с расширением mp3")
+for main, dirs, files in os.walk(rootpath):
+    for filename in fnmatch.filter(files, pattern):
+        # проверка правильности нахождения файлов
+        #print("Название песни: ", filename)
+        listBox.insert('end', filename)
 
 
 root.mainloop()
-
-
-class TestCase(unittest.TestCase):
-
-
-
-    if __name__ == '__main__':
-        unittest.main()
 
 
 
