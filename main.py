24447727384042f3ca3413a78bf09f999eb75abb
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-03-03 23:37:28 +0300

    Commit 6. App is ready.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import fnmatch
 import os
 from pygame import mixer
+from random import randint
 import unittest
 
 # функция запуска песни и присваение тексту название песни
@@ -9,11 +10,15 @@ def select():
     label.config(text=listBox.get("anchor"))
     mixer.music.load(rootpath + "\\" + listBox.get("anchor"))
     mixer.music.play()
+    print("Нажата кнопка Play")
+    print("Сейчас играет: " + listBox.get("anchor"))
 
 # функция остановки музыки и отмены выбора песни
 def stop():
     mixer.music.stop()
     listBox.select_clear('active')
+    print("Нажата кнопка Stop")
+    print("Проигрывание песни остановлено")
 
 # переключение на следующую песню
 def next():
@@ -21,6 +26,8 @@ def next():
     next_song = next_song[0] + 1
     next_song_name = listBox.get(next_song)
     label.config(text = next_song_name)
+    print("Нажата кнопка Next")
+    print("Сейчас играет: " + listBox.get("anchor"))
 
     mixer.music.load(rootpath + "\\" + next_song_name)
     mixer.music.play()
@@ -28,6 +35,40 @@ def next():
     listBox.select_clear(0, 'end')
     listBox.activate(next_song)
     listBox.select_set(next_song)
+
+
+# переключение на предыдущую песню
+def back():
+    cur_song = listBox.curselection()
+    cur_song = cur_song[0] - 1
+    back_song_name = listBox.get(cur_song)
+    label.config(text = back_song_name)
+    print("Нажата кнопка Previous")
+    print("Сейчас играет: " + listBox.get("anchor"))
+
+    mixer.music.load(rootpath + "\\" + back_song_name)
+    mixer.music.play()
+
+    listBox.select_clear(0, 'end')
+    listBox.activate(cur_song)
+    listBox.select_set(cur_song)
+
+
+def mixed():
+    cur_song = listBox.curselection()
+    change = randint(0, 3)
+    cur_song = cur_song[0] + change
+    song_name = listBox.get(cur_song)
+    label.config(text=song_name)
+
+    mixer.music.load(rootpath + "\\" + song_name)
+    mixer.music.play()
+
+    listBox.select_clear(0, 'end')
+    listBox.activate(cur_song)
+    listBox.select_set(cur_song)
+    print("Нажата кнопка Mix")
+    print("Сейчас играет: " + listBox.get("anchor"))
 
 
 
@@ -62,7 +103,7 @@ stop.pack(pady = 15, padx = 10, side = 'left')
 next = tk.Button(root, text = "Next", height = 10, width = 10, command = next)
 next.pack(pady = 15, padx = 10, side = 'left')
 
-mixed = tk.Button(root, text = "Mixed", height = 10, width = 10)
+mixed = tk.Button(root, text = "Mixed", height = 10, width = 10, command = mixed)
 mixed.pack(pady = 15, padx = 10, side = 'left')
 
 
--- mainTest.py
+++
@@ -1,9 +0,0 @@
-from main import *
-
-
-class TestCase(unittest.TestCase):
-
-
-
-    if __name__ == '__main__':
-        unittest.main()
