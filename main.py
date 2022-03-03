46caafb41424d2ae52da9a5d12b6616c8c225f72
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-03-03 23:08:36 +0300

    Commit 5. Added option to play next song by button

--- main.py
+++ main.py
@@ -4,7 +4,35 @@ import os
 from pygame import mixer
 import unittest
 
+# функция запуска песни и присваение тексту название песни
+def select():
+    label.config(text=listBox.get("anchor"))
+    mixer.music.load(rootpath + "\\" + listBox.get("anchor"))
+    mixer.music.play()
 
+# функция остановки музыки и отмены выбора песни
+def stop():
+    mixer.music.stop()
+    listBox.select_clear('active')
+
+# переключение на следующую песню
+def next():
+    next_song = listBox.curselection()
+    next_song = next_song[0] + 1
+    next_song_name = listBox.get(next_song)
+    label.config(text = next_song_name)
+
+    mixer.music.load(rootpath + "\\" + next_song_name)
+    mixer.music.play()
+
+    listBox.select_clear(0, 'end')
+    listBox.activate(next_song)
+    listBox.select_set(next_song)
+
+
+
+
+# инициализация окна
 root = tk.Tk()
 root.title("Music mixer")
 root.geometry("500x400")
@@ -13,23 +41,32 @@ root.config(bg = '#C9C9C9')
 rootpath = "D:\\Downloads\music"
 pattern = "*mp3"
 
+mixer.init()
+
+# настройка элементов формы
 listBox = tk.Listbox(root, width = 100, font = "12")
 listBox.pack(padx = 15, pady = 15)
 
 label = tk.Label(root, text = '', bg = '#C9C9C9',font = "12")
 label.pack()
 
-prev = tk.Button(root, text = "Previous", height = 10, width = 10)
-prev.pack(pady = 15, padx = 20, side = 'left')
+prev = tk.Button(root, text = "Previous", height = 10, width = 10, command = back)
+prev.pack(pady = 15, padx = 10, side = 'left')
 
-play = tk.Button(root, text = "Play", height = 10, width = 10)
-play.pack(pady = 15, padx = 20, side = 'left')
+play = tk.Button(root, text = "Play", height = 10, width = 10, command = select)
+play.pack(pady = 15, padx = 10, side = 'left')
 
-next = tk.Button(root, text = "Next", height = 10, width = 10)
-next.pack(pady = 15, padx = 20, side = 'left')
+stop = tk.Button(root, text = "Stop", height = 10, width = 10, command = stop)
+stop.pack(pady = 15, padx = 10, side = 'left')
 
-mix = tk.Button(root, text = "Mix", height = 10, width = 10)
-mix.pack(pady = 15, padx = 20, side = 'left')
+next = tk.Button(root, text = "Next", height = 10, width = 10, command = next)
+next.pack(pady = 15, padx = 10, side = 'left')
+
+mixed = tk.Button(root, text = "Mixed", height = 10, width = 10)
+mixed.pack(pady = 15, padx = 10, side = 'left')
+
+
+
 
 
 
@@ -46,7 +83,7 @@ for main, dirs, files in os.walk(rootpath):
 
 
 
-
+# зацикливание формы, чтобы не закрылась
 root.mainloop()
 
 
