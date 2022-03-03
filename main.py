4bd1b65f3c977247f5bafea01412a2020890bfe7
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-03-03 21:44:19 +0300

    Commit 2. Preparing main frame and class for testing.

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import fnmatch
+import os
+from pygame import mixer
+import unittest
+
+
+root = tk.Tk()
+root.title("Music mixer")
+root.geometry("600x800")
+
+
+
+root.mainloop()
+
+
+class TestCase(unittest.TestCase):
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
+
+
+
